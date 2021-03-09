1233d7228015c4822b1cc8e7aa87054b795c04ab
Ishwor Giri <ishworgiri1999@gmail.com>
2021-03-09 23:39:36 +0100

    added documentations

--- beergame/frontend/views.py
+++ beergame/frontend/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
+"""
+# All content served using home view using template index.html which can be found inside 
+templates/frontend folder of this current frontend app.
+"""
+
 def home_view(request,*args,**kwargs):
     return render(request,'frontend/index.html')
 
